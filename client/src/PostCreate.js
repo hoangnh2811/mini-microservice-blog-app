b70d1a05b21d346b70e07adcfdeb695ff0ed2b1f
HoangNguyen <hoangnh40@fpt.com.vn>
2022-04-14 15:33:27 +0700

    add ingress-nginx & loadbalance

--- client/src/PostCreate.js
+++ client/src/PostCreate.js
@@ -6,7 +6,7 @@ const PostCreate = (props) => {
 
   const onSubmit = async (e) => {
     e.preventDefault();
-    await axios.post("http://localhost:4000/posts", { title: title });
+    await axios.post("http://posts.com/posts/create", { title: title });
     setTitle("");
   };
 
